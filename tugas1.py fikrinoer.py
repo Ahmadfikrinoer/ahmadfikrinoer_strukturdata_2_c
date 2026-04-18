07a7f91a8dfee0aa58fc304c799f33f4e6605c7d
Ahmadfikrinoer <fikrinoer30@gmail.com>
2026-04-18 13:19:02 +0800

    Add files via upload

---
+++ tugas1.py fikrinoer.py
@@ -0,0 +1,12 @@
+nama = input ("masukan nama")
+NIM = input ("masukan pin nim")
+
+if nama == "ahmad fikri noer":
+    NIM:"30"
+    print("WELCOM FIKRI")
+    print("YES IS MY NAME")
+    print("yes MY NUMBER NIM 25241076")
+else:
+    print("your name no valid")
+    print("soory your NIM IS NO ACSES")
+        

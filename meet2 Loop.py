f00a4c514cd6ad0d5de4db4e8dda8c528b5cd551
Ahmadfikrinoer <fikrinoer30@gmail.com>
2026-04-18 13:19:25 +0800

    Add files via upload

---
+++ meet2 Loop.py
@@ -0,0 +1,16 @@
+# perulangan (loop)
+# for kondisi
+
+angka = [0,1,2,3,4,] # ini menggukan list
+
+for i in angka : 
+    print(f"nilai i = {i}")
+    print("fikri ganteng and kece bet")
+print("program 1 lunas ya pak")
+
+angka = range (1,100) #ini menggunakan range
+
+for i in angka :
+    print (f"nilaii i = {i}")
+    print("nizar manis gk ada obat")
+print("program 2 selse pak")

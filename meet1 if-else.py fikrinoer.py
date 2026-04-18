f7c6a01f4dd2570b1bfe55ee10794c9c52b5bde2
Ahmadfikrinoer <fikrinoer30@gmail.com>
2026-04-18 13:18:31 +0800

    Add files via upload

---
+++ meet1 if-else.py fikrinoer.py
@@ -0,0 +1,35 @@
+#percabanagan
+#kodisi
+#aksi
+
+nama = input ("masukan nama anda")
+
+if nama == "fikri":
+    print("selamat datang fikri")
+    print(" calon mahasiswa comloude")
+elif nama == "dika":
+    print ("hai petinju")
+    print("maafff banget bb anda kurang")
+elif nama == "dira":
+    print("hai dira")
+    print("maaf cinta anda akan terdampat batu")
+else:
+    print("anda bukan petinju")
+    print("bb anda kelebihan")
+print("program selse")
+
+#jika nama yang di input benar, maka akan munculkan nim kalian
+#jika nama yang di inputkan salah. maka akan memunculkan"nama salah"
+
+nama== input ("masukan nama")
+NIM == input ("masukan pin NIM")
+
+if nama== "ahmad fikri noer":
+    NIM:"30"
+    print("WELCOM MY BROTHER")
+    print("NO MY NIM 25241076")
+else:
+    print("you name no valid")
+    print("soory your NIM is aecses")    
+
+
